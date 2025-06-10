4ed626a96a5e2bfcdcecb3ab05b0159a4564cc4d
alan-am <alan.aguilardev@gmail.com>
2025-06-10 16:12:20 -0500

    probando ejemplo swing

--- observer/src/observer/Main.java
+++ observer/src/observer/Main.java
@@ -1,9 +1,41 @@
 package observer;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Quemen Eclipse");
+	
+		        // Crear la ventana
+		        JFrame ventana = new JFrame("Implementando AOP y Observer");
+		        ventana.setSize(400, 300);
+		        
+		        //seteado de terminar ejecucion al cerrar ventana
+		        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		        
+		        ventana.setLayout(new FlowLayout());
+		        
+		        // Crear panel para cambiar el color
+		        JPanel panel = new JPanel();
+		        panel.setPreferredSize(new Dimension(250, 100));
+
+		        // Crear botón
+		        JButton boton = new JButton("Cambiar color");
+
+		        // Acción al hacer clic
+		        boton.addActionListener(e -> {
+		            panel.setBackground(Color.YELLOW);
+		            System.out.println("Fondo cambiado a AMARILLO");
+		        });
+
+		        // Agregar botón y panel a la ventana
+		        ventana.add(boton);
+		        ventana.add(panel);
+
+		        // Mostrar ventana
+		        ventana.setVisible(true);
 
 	}
 
